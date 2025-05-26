ae5c060c67407e415469aa18dd50028ffac58f16
Pham Van Huong <dolataoday123@gmail.com>
2025-05-26 17:54:35 +0700

    api update,detail company

--- backend/controller/client/company.controller.js
+++ backend/controller/client/company.controller.js
@@ -18,7 +18,23 @@ module.exports.getAllCompany = async (req, res) => {
         })
   }
 };
-
+// Chi tiet thong cong ty
+module.exports.detailCompany = async (req,res) =>{
+  const id = req.params.id;
+  try{
+    const company = await Company.findOne({_id : id});
+    res.json({
+      status : 200,
+      message : "Da tim thay",
+      data : company
+    })
+  }catch(error){
+    res.status(500).json({
+      status : 500,
+      message : "Khoong tim thay"
+    })
+  }
+}
 // Thêm thông tin công ty
 module.exports.addCompany = async (req,res) =>{
   const company = new Company(req.body)
@@ -27,14 +43,14 @@ module.exports.addCompany = async (req,res) =>{
     await company.save();
   res.json({
     status : 200,
-    message : "Thêm thành công",
+    message : "create Successfull",
     data : company
   });
   }
   catch{
     res.status(500).json({
       status : 500,
-      message : "Thêm thất bại"
+      message : "create falied"
     })
   }
 }
@@ -45,14 +61,35 @@ module.exports.deleteCompany =async (req,res) =>{
     await Company.updateOne({_id : id},{deleted : true})
     res.json({
       status : 200,
-      message : "Xoa thanh cong",
+      message : "Deleted successful",
 
     })
   }
   catch(error){
     res.status(500).json({
       status : 500,
-      message : "Xoa that bai"
+      message : `deleted failed ${error}`
+    })
+  }
+}
+
+// Cap nhat thong tin
+module.exports.updateCompany =async (req,res)=>{
+
+  const id = req.params.id
+  try{
+
+    await Company.updateOne({_id : id}, req.body)
+    res.status(200).json({
+      status : 200,
+      message : "Update Successful",
+      data : req.body
+    })
+  }
+  catch(error){
+    res.status(500).json({
+      status : 500,
+      message : `update failed ${error}`
     })
   }
 }
--- backend/routes/client/company.route.js
+++ backend/routes/client/company.route.js
@@ -5,9 +5,16 @@ const companyController = require('../../controller/client/company.controller')
 // [Get] api/companies
 route.get('/companies',companyController.getAllCompany);
 
+// [Get] api/companies/:id
+route.get('/companies/:id',companyController.detailCompany);
+
 // [POST] api/companies/add (Thêm thông tin công ty)
 route.post("/companies/add",companyController.addCompany)
 
 // [Delete] api/companies/delete/:id (Xoa cong ty)
 route.delete("/companies/delete/:id",companyController.deleteCompany)
+
+// [Patch]api/companies/update/:id
+route.patch("/companies/update/:id",companyController.updateCompany)
+
 module.exports = route
