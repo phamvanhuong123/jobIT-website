cbd3bfc6eb4511e09473825432f44a3a78218b96
Pham Van Huong <dolataoday123@gmail.com>
2025-05-29 08:45:33 +0700

    backend update job

--- backend/controller/job.controller.js
+++ backend/controller/job.controller.js
@@ -1,6 +1,29 @@
 
 const Job = require("../model/job.model");
 
+// Lấy danh sách job theo công ty
+module.exports.getJobsbyCompany = async (req,res) =>{
+    try{
+        const idCompany = req.params.idCompany;
+        const find  = {
+            deleted : false,
+            idCompany : idCompany
+
+        }
+        const record = await Job.find(find);
+        res.json({
+            status : 200,
+            message: "Thanh cong",
+            data : record
+    })
+    }
+    catch(e){
+        res.status(500).json({
+            status : 500,
+            message : `Thất bại : ${e}`
+        })
+    }
+}
 
 // Lấy danh sách các job
 module.exports.getAllJobs = async (req,res) =>{
@@ -10,6 +33,7 @@ module.exports.getAllJobs = async (req,res) =>{
         }
         const record = await Job.find(find);
         res.json({
+        status : 200,
         message: "Thanh cong",
         data : record
     })
@@ -23,8 +47,6 @@ module.exports.getAllJobs = async (req,res) =>{
 }
 // Tạo job mới theo công ty.
 module.exports.createJoByCompany =async (req,res) =>{
-    
-    
     try{
         const idCompany = req.params.idCompany;
         req.body.idCompany = idCompany;
@@ -57,6 +79,24 @@ module.exports.deleteJob = async (req,res) =>{
          res.status(500).json({
             status : 500,
             message : `failed : ${e}`
+        })
+    }
+}
+// Cật nhật thông tin job
+module.exports.updateJob = async (req,res) => {
+    try{
+        const id = req.params.id;
+        await Job.updateOne({_id : id}, {$set : req.body})
+        res.json({
+            status : 200,
+            message : "update successful",
+            data : req.body
+        })
+    }
+    catch{
+        res.status(500).json({
+            status : 500,
+            message : "update failed",
         })
     }
 }
--- backend/routes/job.route.js
+++ backend/routes/job.route.js
@@ -4,10 +4,16 @@ const jobController = require('../controller/job.controller');
 // [Get] api/jobs
 route.get('/jobs',jobController.getAllJobs);
 
+// [GET] api/jobs/:idCompany
+route.get("/jobs/:idCompany",jobController.getJobsbyCompany)
+
 // [Post] api/jobs/create/:idCompany
 route.post('/jobs/create/:idCompany',jobController.createJoByCompany)
 
 // [Delete] api/jobs/delete/:id
 route.delete('/jobs/delete/:id',jobController.deleteJob)
+
+// [PATCH] api/jobs/update/:id
+route.patch("/jobs/update/:id",jobController.updateJob)
 
 module.exports = route;
