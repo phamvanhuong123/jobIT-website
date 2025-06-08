ce14eaa75e8cbb80113ddce60ec7428d77c2acb8
Pham Van Huong <dolataoday123@gmail.com>
2025-06-08 11:30:46 +0700

    fix api company

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
 const port = dotenv.parsed.POR || 5000;
-const sendEmail = require('./utils/sendEmail')
-sendEmail()
+
 // cấu hình cors
 const cors = require('cors');
 app.use(cors())
