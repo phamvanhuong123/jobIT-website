3f4e78bf9d68b31719d5f9628b343a9bed2bbf21
Pham Van Huong <dolataoday123@gmail.com>
2025-06-14 11:06:24 +0700

    update model candidate and page account SavedJob

--- frontend/src/pages/Account/MyJobs/MyJobs.tsx
+++ frontend/src/pages/Account/MyJobs/MyJobs.tsx
@@ -1,6 +1,38 @@
-function MyJobs(){
-    return <>
-    Đây là trang MyJobs
-    </>
+import {  Tabs } from "antd";
+import type { TabsProps } from "antd";
+import SavedJob from "./SavedJob/SavedJob";
+function MyJobs() {
+  const items: TabsProps["items"] = [
+    {
+      key: "1",
+      label: "Đã ứng tuyển",
+        children : 'danh sahcs 3'
+
+    },
+    {
+        key: '2',
+        label : 'Đã lưu',
+        children : <SavedJob/>
+    },
+    {
+        key : '3',
+        label : 'Xem gần đây',
+        children : 'danh sahcs 2'
+
+    }
+  ];
+  const renderTabBar: TabsProps['renderTabBar'] = (props,DefaultTabBar) =>(
+    <div style={{backgroundColor : 'white',padding : 12,borderRadius : 10, height :100}}>
+        <h3>Việc làm của tôi</h3>
+        <DefaultTabBar {...props} style={{height : 63}} />
+    </div>
+  )
+
+  return <>
+   
+        <Tabs items={items} renderTabBar={renderTabBar} />
+    
+  
+  </>;
 }
-export default MyJobs
+export default MyJobs;
---
+++ frontend/src/pages/Account/MyJobs/SavedJob/SavedJob.tsx
@@ -0,0 +1,10 @@
+import { Card } from "antd"
+
+function SavedJob(){
+    return <>
+        <Card style={{marginTop : 20}}>
+            Danh sách lưu công việc
+        </Card>
+    </>
+}
+export default SavedJob
