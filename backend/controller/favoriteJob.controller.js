e78721464e11af2663e1553225500b94d6e95020
Pham Van Huong <dolataoday123@gmail.com>
2025-06-16 09:59:25 +0700

    update api favoriteJob

--- backend/controller/favoriteJob.controller.js
+++ backend/controller/favoriteJob.controller.js
@@ -15,11 +15,67 @@ module.exports.getAllFavoriteJobsByIdCandidate = async (req, res) => {
       return;
     }
 
-    const jobIds = await FavoriteJob.find({ idCandidate: idCandidate }).sort({
-      idJob: "desc",
-    });
-    const ids = jobIds.map((item) => item.idJob);
-    const record = await Job.find({ _id: { $in: ids } });
+    // const jobIds = await FavoriteJob.find({ idCandidate: idCandidate }).sort({
+    //   idJob: "desc",
+    // });
+    // const ids = jobIds.map((item) => item.idJob);
+    // const record = await Job.find({ _id: { $in: ids } });
+    const record = await FavoriteJob.aggregate([
+      {
+        $match: { idCandidate: new mongoose.Types.ObjectId(idCandidate) },
+      },
+      {
+        $lookup: {
+          from: "jobs",
+          localField: "idJob",
+          foreignField: "_id",
+          as: "job",
+        },
+      },
+      {
+        $unwind: {
+          path: "$job",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $addFields: {
+          "job.ObjectIdCompany": { $toObjectId: "$job.idCompany" },
+        },
+      },
+      {
+        $lookup: {
+          from: "companies",
+          localField: "job.ObjectIdCompany",
+          foreignField: "_id",
+          as: "company",
+        },
+      },
+      {
+        $unwind: {
+          path: "$company",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $project: {
+          idCandidate: 1,
+          idJob: 1,
+          createdAt: 1,
+          updatedAt: 1,
+          "job.idCompany": 1,
+          "job.name": 1,
+          "job.logo": 1,
+          "job.locations": 1,
+          "job.workplace": 1,
+          "job.salary": 1,
+          "job.createdAt": 1,
+          "job.updatedAt": 1,
+          "company.name": 1,
+          "company.logo": 1,
+        },
+      },
+    ]);
 
     res.json({
       status: 200,
@@ -94,22 +150,18 @@ module.exports.addFavoriteJob = async (req, res) => {
 // Xoá công việc yêu thích
 module.exports.deleteFavoriteJob = async (req, res) => {
   try {
-    const { idCandidate, idJob } = req.params;
+    const { id } = req.params;
     // Kiểm tra ObjectId có hợp lệ hay không
-    if (
-      !mongoose.Types.ObjectId.isValid(idCandidate) ||
-      !mongoose.Types.ObjectId.isValid(idJob)
-    ) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
       res.status(400).json({
         status: 400,
-        message: "IdCandidate hoặc IdJob không hợp lệ",
+        message: "Id không hợp lệ",
       });
       return;
     }
     // Xoá bản ghi
     const count = await FavoriteJob.deleteOne({
-      idCandidate: idCandidate,
-      idJob: idJob,
+      _id : id
     });
     if (count.deletedCount == 0) {
       res.status(400).json({
--- backend/routes/favoriteJob.route.js
+++ backend/routes/favoriteJob.route.js
@@ -3,6 +3,6 @@ const route = express.Router();
 const favoriteJobController = require('../controller/favoriteJob.controller')
 route.get('/favorite-jobs/:idCandidate',favoriteJobController.getAllFavoriteJobsByIdCandidate)
 route.post('/favorite-jobs/add',favoriteJobController.addFavoriteJob)
-route.delete('/favorite-jobs/delete/:idCandidate/:idJob',favoriteJobController.deleteFavoriteJob)
+route.delete('/favorite-jobs/delete/:id',favoriteJobController.deleteFavoriteJob)
 
 module.exports = route
