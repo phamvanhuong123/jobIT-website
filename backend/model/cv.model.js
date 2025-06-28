3184a725231d668a1aa957cfca1146e57dcb0511
Pham Van Huong <dolataoday123@gmail.com>
2025-06-28 23:02:23 +0700

    model Cv

--- backend/model/cv.model.js
+++ backend/model/cv.model.js
@@ -1,10 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const CvSchema = new mongoose.Schema({
-    idUser : String,
-    fullName : String,
-    
-
-},{
-    timestamps : true
-})
+const CvSchema = new mongoose.Schema(
+  {
+    idUser: String,
+    idJob: String,
+    fullName: String,
+    phone: string,
+    locations: [String],
+    coverLetter: string,
+    cvUrl: string,
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "rejected"],
+    },
+    isRead: boolean, 
+  },
+  {
+    timestamps: true,
+  }
+);
+const Cv = mongoose.model("cvs",CvSchema);
+module.exports = Cv
